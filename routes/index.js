a3d071f660b64ddf249689d85b32ff9320c6b49e
dinarfahdil <dinarfahdilamin09@gmail.com>
2022-10-03 19:33:18 +0700

    dinar first api

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/mhs.json')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.json(
-     { message:"Welcome to 1957301007 API"}
+    { message: "Welcome Dinar API" }
   );
+});
+
+router.get('/mahasiswa', function (req, res, next) {
+  res.status(200).send(data)
 });
 
 module.exports = router;
